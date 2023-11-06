abc1564ef2096f45e6e97305ed9573d197065cf6
Nayak <rahul.nyk@gmail.com>
2023-11-06 18:57:01 +0530

    minor enhancements

--- chains_v2/refine_answer.py
+++ chains_v2/refine_answer.py
@@ -13,12 +13,14 @@ class RefineAnswer(LLMChain):
     def from_llm(cls, llm: BaseLanguageModel, verbose: bool = True) -> LLMChain:
         """Get the response parser."""
         prompt_template = (
-            "Your are provided with a question and an exsiting answer.\n"
-            " Question: '{question}'\nExisting Answer: \n----\n{answer}\n----\n"
-            " Your team has provided you with some additional context that may be relevant to the question."
-            " New Context: \n----\n{context}\n----\n"
+            "Your task is to answer the following question. and an exsiting answer.\n"
+            " Question: '{question}'\n"
+            " You are provided with an existing Answer: \n---\n{answer}\n---\n\n"
+            " You are also provided with some additional context that may be relevant to the question.\n"
+            " New Context: \n---\n{context}\n---\n\n"
             " You have the opportunity to rewrite and improve upon the existing answer."
-            " Use only the information from the existing answer and the context to write better answer."
+            " Use only the information from the existing answer and the given context to write better answer."
+            " Use a descriptive style and a business casual language."
             " If the context isn't useful, return the existing answer."
         )
         prompt = PromptTemplate(
